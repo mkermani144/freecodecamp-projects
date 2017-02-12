b5c1ff42fd5a51359f44855afc15215744758fad
mohammad <mkermani144@gmail.com>
2017-02-12 09:15:54 +0330

    Design dialog interface

--- recipe-box/src/RecipeBox.js
+++ recipe-box/src/RecipeBox.js
@@ -2,19 +2,64 @@ import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 import { green900, blue500 } from 'material-ui/styles/colors';
 import './RecipeBox.css';
 
 class RecipeBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogOpen: false
+    };
+  }
   render() {
+    const blueStyle = {
+      color: blue500,
+      borderColor: blue500
+    };
+    const dialogActions = [
+      <FlatButton
+        label="Cancel"
+        style={blueStyle}
+      />,
+      <FlatButton
+        label="Add"
+        style={blueStyle}
+        keyboardFocused={true}
+      />,
+    ];
     return (
       <div className="recipebox" style={{backgroundColor: green900}}>
         <Paper className="box">
 
         </Paper>
-        <FloatingActionButton className="fab" backgroundColor={blue500}>
+        <FloatingActionButton className="fab" backgroundColor={blue500}
+          onClick={() => this.setState({ dialogOpen: true })}>
           <ContentAdd />
         </FloatingActionButton>
+        <Dialog open={this.state.dialogOpen}
+          actions={dialogActions}
+          title='Add new recipe'
+          onRequestClose={buttonClicked => this.setState({ dialogOpen: false })}
+        >
+          <TextField
+            floatingLabelText="Recipe name"
+            hintText="Spaghetti"
+            fullWidth={true}
+            underlineFocusStyle={blueStyle}
+            floatingLabelFocusStyle={blueStyle}
+          />
+          <TextField
+            floatingLabelText="Ingredients"
+            hintText="Noodles,Tomato Sauce,(Optional) Meatballs"
+            fullWidth={true}
+            underlineFocusStyle={blueStyle}
+            floatingLabelFocusStyle={blueStyle}
+          />
+        </Dialog>
       </div>
     );
   }
--- recipe-box/src/index.js
+++ recipe-box/src/index.js
@@ -1,7 +1,10 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
