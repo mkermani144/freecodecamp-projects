37a7b254bd696725a6a5969fb34502433043cdcd
mohammad <mkermani144@gmail.com>
2017-02-14 12:21:55 +0330

    Add method to generate random game of life board

--- game-of-life/src/BoardContainer.js
+++ game-of-life/src/BoardContainer.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class BoardContainer extends Component {
   constructor() {
-    
+
+  }
+  randomArray = () => {
+    array = [];
+    for (let i = 0; i < 10; i++) {
+      array[i] = [];
+      for (let j = 0; j < 10; j++) {
+        array[i][j] = Math.floor(Math.random() + .5);
+      }
+    }
+    return array;
   }
 }
 
