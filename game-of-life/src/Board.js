f3f0e1fa81d18784c47a1815473f513cb79ab20f
mohammad <mkermani144@gmail.com>
2017-02-15 16:59:14 +0330

    Add Board

--- game-of-life/src/Board.js
+++ game-of-life/src/Board.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import Cell from './Cell';
 
 class Board extends Component {
-
+  render() {
+    const cells = [];
+    for (let i = 0; i < 100; i++) {
+      const bgColor = this.props.states[i] == 1 ? 'red' : 'black';
+      cells.push(<Cell onClick={this.props.onClick} style={{backgroundColor: bgColor}} key={i} />);
+    }
+    return <div>{cells}</div>;
+  }
 }
 
 export default Board;
