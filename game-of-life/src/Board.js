b67a34fa4674da979ca24697bf538b22572d98c3
mohammad <mkermani144@gmail.com>
2017-02-16 11:58:14 +0330

    Fix eslint warning

--- game-of-life/src/Board.js
+++ game-of-life/src/Board.js
@@ -5,7 +5,7 @@ class Board extends Component {
   render() {
     const cells = [];
     for (let i = 0; i < 100; i++) {
-      const bgColor = this.props.states[i] == 1 ? 'red' : 'black';
+      const bgColor = this.props.states[i] === 1 ? 'red' : 'black';
       cells.push(<Cell onClick={this.props.onClick} style={{backgroundColor: bgColor}} key={i} />);
     }
     return <div>{cells}</div>;
