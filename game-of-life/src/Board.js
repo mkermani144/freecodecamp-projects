293ab882c914efef06a4f05e328f9564d0cbd67e
mohammad <mkermani144@gmail.com>
2017-02-16 12:10:19 +0330

    Update styles

--- game-of-life/src/Board.js
+++ game-of-life/src/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cell from './Cell';
+import './Board.css';
 
 class Board extends Component {
   render() {
@@ -8,7 +9,7 @@ class Board extends Component {
       const bgColor = this.props.states[i] === 1 ? 'red' : 'black';
       cells.push(<Cell onClick={this.props.onClick} style={{backgroundColor: bgColor}} key={i} />);
     }
-    return <div>{cells}</div>;
+    return <div className="board">{cells}</div>;
   }
 }
 
