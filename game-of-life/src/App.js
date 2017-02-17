6feaa628aad93098ceb7aa6d84d845676d0ad7c6
mohammad <mkermani144@gmail.com>
2017-02-17 15:07:50 +0330

    Add BoardContainer to App

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import BoardContainer from './BoardContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <BoardContainer />
       </div>
     );
   }
