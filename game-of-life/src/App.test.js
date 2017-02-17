7e38133f03310fbd7010364ade4b2d0098276f62
mohammad <mkermani144@gmail.com>
2017-02-17 11:59:44 +0330

    Add basic App test

--- game-of-life/src/App.test.js
+++ game-of-life/src/App.test.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+  it('has a board within', () => {
+    const action = {
+      command: 'start',
+      speed: 1,
+      auto: true
+    };
+    const wrapper = shallow(<App action={'start'} reportGenerationMumber={undefined} />);
+    expect(wrapper.containsMatchingElement(<div class=".board" />)).toBeTrue();
+  });
+  it('has a toggle', () => {
+    const action = {
+      command: 'start',
+      speed: 1,
+      auto: true
+    };
+    const wrapper = shallow(<App action={'start'} reportGenerationMumber={undefined} />);
+    expect(wrapper.containsMatchingElement('.toggle')).toBeTrue();
+  });
+  it('has three buttons', () => {
+    const action = {
+      command: 'start',
+      speed: 1,
+      auto: true
+    };
+    const wrapper = shallow(<App action={'start'} reportGenerationMumber={undefined} />);
+    expect(wrapper.find(<Button />)).toHaveLength(3);
+  });
+  it('has generation textfield', () => {
+    const action = {
+      command: 'start',
+      speed: 1,
+      auto: true
+    };
+    const wrapper = shallow(<App action={'start'} reportGenerationMumber={undefined} />);
+    expect(wrapper.containsMatchingElement(<div className="generationNumber" />)).toBeTrue();
+  });
 });
--- game-of-life/src/BoardContainer.js
+++ game-of-life/src/BoardContainer.js
@@ -7,6 +7,7 @@ class BoardContainer extends Component {
     this.state = {
       states: this.randomArray()
     };
+    setInterval(this.update, 1000);
   }
   render() {
     return <Board states={this.state.states} onClick={this.handleClick} />;
@@ -22,6 +23,38 @@ class BoardContainer extends Component {
       array[i] = Math.floor(Math.random() + .4);
     }
     return array;
+  }
+  update = () => {
+    const nextGeneration = [];
+    const neighbours = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+    for (let i = 0; i < 40; i++) {
+      for (let j = 0; j < 40; j++) {
+        let total = 0;
+        neighbours.forEach((neighbour) => {
+          if(i + neighbour[0] > 39 || i + neighbour[0] < 0 || j + neighbour[1] > 39 || j + neighbour[1] < 0) {
+            total += 0;
+          } else {
+            total += this.state.states[(i + neighbour[0]) * 40 + (j + neighbour[1])];
+          }
+        });
+        if (this.state.states[i * 40 + j] === 1) {
+          if (total < 2 || total > 3) {
+            nextGeneration[i * 40 + j] = 0;
+          } else {
+            nextGeneration[i * 40 + j] = 1;
+          }
+        } else {
+          if (total === 3) {
+            nextGeneration[i * 40 + j] = 1;
+          } else {
+            nextGeneration[i * 40 + j] = 0;
+          }
+        }
+      }
+    }
+    this.setState({
+      states: nextGeneration
+    });
   }
 }
 
