716a5a230a2843e9bfc86214ffee428614641479
mohammad <mkermani144@gmail.com>
2017-02-19 08:50:06 +0330

    Add dirtyBattlefield method

--- roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
+++ roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import Battlefield from './Battlefield';
 
 class BattlefieldContainer extends Component {
+  constructor() {
+    this.cellsInfo = Battlefield[0];
+    this.dirtyBattlefield();
+  }
   render() {
+    const cellsInfo = null;
     return <Battlefield />;
+  }
+  dirtyBattlefield = () => {
+    const base = (dungeon, type, iterateNum) => {
+      while (iterateNum) {
+        const row = Math.floor(Math.random()) * 400;
+        const column = Math.floor(Math.random()) * 400;
+        if (this.cellsInfo[dungeon][row][column] === 1) {
+          this.cellsInfo[dungeon][row][column] = type;
+          iterateNum--;
+        }
+      }
+    }
+    const shuffleEnemies = (dungeon) => base(dungeon, 2, 5);
+    const shuffleHealthItems = (dungeon) => base(dungeon, 3, 5);
+    const shuffleWeapon = (dungeon) => base(dungeon, 4, 1);
+    const shuffleLineOfSightEnhancer = (dungeon) => base(dungeon, 5, 1);
+    for (let dungeon = 0; dungeon < 5; dungeon++) {
+      shuffleEnemies(dungeon);
+      shuffleHealthItems(dungeon);
+      shuffleWeapon(dungeon);
+      shuffleLineOfSightEnhancer(dungeon);
+    }
   }
 }
 
--- roughlike-dungeon-crawler-game/src/BattlefieldInfo.js
+++ roughlike-dungeon-crawler-game/src/BattlefieldInfo.js
@@ -1,3 +1,11 @@
+/**
+ * 0: Blocked
+ * 1: Battlefield
+ * 2: Enemy
+ * 3: Health item
+ * 4: Weapon
+ * 5: Line of sight enhancer
+ */
 const BattlefieldInfo = [
   [],
   [],
