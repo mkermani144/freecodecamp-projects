20d5cd4b98a5a6ff16a19f0e6f059364562700a0
mohammad <mkermani144@gmail.com>
2017-03-04 09:10:39 +0330

    Add basic key stroke handling

--- roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
+++ roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
@@ -7,13 +7,18 @@ import unzip from 'lodash/unzip';
 class BattlefieldContainer extends Component {
   constructor() {
     super();
-    this.cellsInfo = [createMap()];
-    this.playerPos = {
-      x: 0,
-      y: 0
+    this.state = {
+      playerPos: {
+        x: 0,
+        y: 0
+      },
+      cellsInfo: [createMap()]
     };
     this.lineOfSight = 10;
     this.dirtyBattlefield();
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
   }
   render() {
     const typeToColor = {
@@ -32,7 +37,7 @@ class BattlefieldContainer extends Component {
       submap[i] = [];
       for (let j = 0; j <= 50; j++) {
         if ((i - 50) * (i - 50) + (j - 25) * (j - 25) < this.lineOfSight * this.lineOfSight) {
-          submap[i][j] = this.cellsInfo[0][i + (this.playerPos.x - 50)][j + (this.playerPos.y - 25)];
+          submap[i][j] = this.state.cellsInfo[0][i + (this.state.playerPos.x - 50)][j + (this.state.playerPos.y - 25)];
         } else {
           submap[i][j] = -1;
         }
@@ -45,8 +50,8 @@ class BattlefieldContainer extends Component {
       while (iterateNum) {
         const row = Math.floor(Math.random() * 200);
         const column = Math.floor(Math.random() * 100);
-        if (this.cellsInfo[dungeon][row][column] === 1) {
-          this.cellsInfo[dungeon][row][column] = type;
+        if (this.state.cellsInfo[dungeon][row][column] === 1) {
+          this.state.cellsInfo[dungeon][row][column] = type;
           iterateNum--;
           if (type === 7) {
             return {
@@ -63,13 +68,87 @@ class BattlefieldContainer extends Component {
     const shuffleLineOfSightEnhancer = (dungeon) => base(dungeon, 5, 1);
     const shufflePortal = (dungeon) => base(dungeon, 6, 1);
     const shufflePlayer = (dungeon) => base(dungeon, 7, 1);
-    this.playerPos = shufflePlayer(0);
+    this.state.playerPos = shufflePlayer(0);
     for (let dungeon = 0; dungeon < 1; dungeon++) {
       shuffleEnemies(dungeon);
       shuffleHealthItems(dungeon);
       shuffleWeapon(dungeon);
       shuffleLineOfSightEnhancer(dungeon);
       shufflePortal(dungeon);
+    }
+  }
+  handleKeydown = (event) => {
+    this.state.cellsInfo[0][this.state.playerPos.x][this.state.playerPos.y] = 1;
+    switch (event.keyCode) {
+      case 37:
+        this.setState((prev) => {
+          return {
+            playerPos: {
+              x: prev.playerPos.x - 1,
+              y: prev.playerPos.y
+            }
+          };
+        }, () => {
+          this.setState((prev) => {
+            prev.cellsInfo[0][this.state.playerPos.x][this.state.playerPos.y] = 7;
+            return {
+              cellsInfo: prev.cellsInfo
+            };
+          })
+        });
+        break;
+      case 38:
+        this.setState((prev) => {
+          return {
+            playerPos: {
+              x: prev.playerPos.x,
+              y: prev.playerPos.y - 1
+            }
+          };
+        }, () => {
+          this.setState((prev) => {
+            prev.cellsInfo[0][this.state.playerPos.x][this.state.playerPos.y] = 7;
+            return {
+              cellsInfo: prev.cellsInfo
+            };
+          })
+        });
+        break;
+      case 39:
+        this.setState((prev) => {
+          return {
+            playerPos: {
+              x: prev.playerPos.x + 1,
+              y: prev.playerPos.y
+            }
+          };
+        }, () => {
+          this.setState((prev) => {
+            prev.cellsInfo[0][this.state.playerPos.x][this.state.playerPos.y] = 7;
+            return {
+              cellsInfo: prev.cellsInfo
+            };
+          })
+        });
+        break;
+      case 40:
+        this.setState((prev) => {
+          return {
+            playerPos: {
+              x: prev.playerPos.x,
+              y: prev.playerPos.y + 1
+            }
+          };
+        }, () => {
+          this.setState((prev) => {
+            prev.cellsInfo[0][this.state.playerPos.x][this.state.playerPos.y] = 7;
+            return {
+              cellsInfo: prev.cellsInfo
+            };
+          })
+        });
+        break;
+      default:
     }
   }
 }
