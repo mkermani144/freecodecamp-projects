1a2a85a45384bc77862b610f3c5bcef6d0b7ec1a
mohammad <mkermani144@gmail.com>
2017-03-05 08:57:59 +0330

    Handle picking the weapon up basically

--- roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
+++ roughlike-dungeon-crawler-game/src/BattlefieldContainer.js
@@ -170,10 +170,19 @@ class BattlefieldContainer extends Component {
       case 3:
         const healthItem = this.state.items[this.state.dungeon][[x, y]];
         this.setState(prev => {
-          console.log(this.state.playerHealth);
           prev.cellsInfo[this.state.dungeon][this.state.playerPos.x][this.state.playerPos.y] = 1;
           prev.cellsInfo[this.state.dungeon][x][y] = 7;
           prev.playerHealth += healthItem.heal;
+          prev.playerPos = {x, y};
+          return prev;
+        });
+        break;
+      case 4:
+        const weapon = this.state.items[this.state.dungeon][[x, y]];
+        this.setState(prev => {
+          prev.cellsInfo[this.state.dungeon][this.state.playerPos.x][this.state.playerPos.y] = 1;
+          prev.cellsInfo[this.state.dungeon][x][y] = 7;
+          prev.playerDamage += weapon.damageIncrease;
           prev.playerPos = {x, y};
           return prev;
         });
