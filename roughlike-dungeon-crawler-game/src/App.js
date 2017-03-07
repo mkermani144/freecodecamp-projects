f7f58cc33be555bab49203bf2a2a52ac6e40a62a
mohammad <mkermani144@gmail.com>
2017-03-07 08:47:21 +0330

    Add title

--- roughlike-dungeon-crawler-game/src/App.js
+++ roughlike-dungeon-crawler-game/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import BattlefieldContainer from './BattlefieldContainer';
 import HudContainer from './HudContainer';
 import Title from './Title';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <Title />
         <HudContainer />
         <BattlefieldContainer />
--- roughlike-dungeon-crawler-game/src/Title.js
+++ roughlike-dungeon-crawler-game/src/Title.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './Title.css';
 
 class Title extends Component {
   render() {
-    return null;
+    return <h1 className="title">Roughlike dungeon crawler game</h1>;
   }
 }
 
