25122e25f700cb678fad8a97f8f72bbf815f14a3
mohammad <mkermani144@gmail.com>
2017-03-07 09:00:41 +0330

    Add hud

--- roughlike-dungeon-crawler-game/src/Hud.js
+++ roughlike-dungeon-crawler-game/src/Hud.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import './Hud.css';
 
 class Hud extends Component {
   render() {
-    return null;
+    return (
+      <div className="hud">
+        <p style={{color: '#00ff00'}}>Health: {this.props.health} {'\u00A0'}{'\u00A0'}</p>
+        <p style={{color: 'red'}}>Damage: {this.props.damage} {'\u00A0'}{'\u00A0'}</p>
+        <p style={{color: '#2288ff'}}>Level: {this.props.level} {'\u00A0'}{'\u00A0'}</p>
+        <p style={{color: 'yellow'}}>XP: {this.props.xp} {'\u00A0'}{'\u00A0'}</p>
+        <p style={{color: '#8a2be2'}}>Dungeon: {this.props.dungeon} {'\u00A0'}{'\u00A0'}</p>
+      </div>
+    );
   }
 }
 
