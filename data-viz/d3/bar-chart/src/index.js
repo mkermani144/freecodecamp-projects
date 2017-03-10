b1a37479284745130bd56eb208384db2bda6cfb2
mohammad <mkermani144@gmail.com>
2017-03-10 19:10:45 +0330

    Use d3 scales

--- data-viz/d3/bar-chart/src/index.js
+++ data-viz/d3/bar-chart/src/index.js
@@ -2,9 +2,13 @@ import * as d3 from 'd3';
 import '../public/index.scss';
 import json from './GDP-data.json';
 
+const x = d3.scaleTime()
+                  .domain([json.data[0][1], json.data[json.data.length - 1][1]])
+                  .range([0, 100]);
+
 d3.select('.bar-chart')
   .selectAll('div')
   .data(json.data)
   .enter()
   .append('div')
-  .style('height', (el) => el[1] * 100 / d3.max(json.data, (d) => d[1]) + '%');
+  .style('height', (el) => x(el[1]) + '%');
