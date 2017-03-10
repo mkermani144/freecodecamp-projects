2004e5df8299adde61d263c896ea8e6e65655af4
mohammad <mkermani144@gmail.com>
2017-03-10 17:51:48 +0330

    Add initial chart

--- data-viz/d3/bar-chart/src/index.js
+++ data-viz/d3/bar-chart/src/index.js
@@ -1,1 +1,10 @@
+import * as d3 from 'd3';
+import '../public/index.scss';
+import json from './GDP-data.json';
 
+d3.select('.bar-chart')
+  .selectAll('div')
+  .data(json.data)
+  .enter()
+  .append('div')
+  .style('height', (el) => el[1] * 100 / d3.max(json.data, (d) => d[1]) + '%');
