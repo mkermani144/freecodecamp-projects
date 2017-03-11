74a50b57e768933f2d928f66befe205ed120a16f
mohammad <mkermani144@gmail.com>
2017-03-11 10:24:04 +0330

    Use svg

--- data-viz/d3/bar-chart/src/index.js
+++ data-viz/d3/bar-chart/src/index.js
@@ -2,13 +2,32 @@ import * as d3 from 'd3';
 import '../public/index.scss';
 import json from './GDP-data.json';
 
-const x = d3.scaleLinear()
+const time = d3.scaleTime()
+  .domain([new Date(json.data[0][0]), new Date(json.data[json.data.length - 1][0])])
+  .range([40, 800]);
+const gdp = d3.scaleLinear()
   .domain([json.data[0][1], json.data[json.data.length - 1][1]])
-  .range([0, 100]).nice();
+  .range([580, 0]);
+
+const xAxis = d3.axisBottom().scale(time);
+const yAxis = d3.axisLeft().scale(gdp).ticks(15, 's');
 
 d3.select('.bar-chart')
-  .selectAll('div')
+  .selectAll('g')
   .data(json.data)
   .enter()
-  .append('div')
-  .style('height', (el) => x(el[1]) + '%');
+  .append('g')
+  .attr('transform', el => `translate(${time(new Date(el[0]))}, 0)`)
+  .append('rect')
+  .attr('width', (800 / json.data.length) + 1)
+  .attr('height', el => 580 - gdp(el[1]))
+  .attr('y', el => gdp(el[1]));
+
+d3.select('.bar-chart')
+  .append('g')
+  .attr('transform', 'translate(0, 580)')
+  .call(xAxis);
+d3.select('.bar-chart')
+  .append('g')
+  .attr('transform', 'translate(40, 0)')
+  .call(yAxis);
