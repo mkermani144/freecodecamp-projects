13728ade71475baade3d2cae248578c5f8705de5
mohammad <mkermani144@gmail.com>
2017-03-11 10:54:13 +0330

    Add tooltip

--- data-viz/d3/bar-chart/src/index.js
+++ data-viz/d3/bar-chart/src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import d3Tip from 'd3-tip';
 import '../public/index.scss';
 import json from './GDP-data.json';
 
@@ -12,12 +13,16 @@ const gdp = d3.scaleLinear()
 const xAxis = d3.axisBottom().scale(time);
 const yAxis = d3.axisLeft().scale(gdp).ticks(15, 's');
 const svg = d3.select('.bar-chart');
-
+const tip = d3Tip().attr('class', 'd3-tip').html(d => {
+  return `<div class="tip-gdp">\$${d[1]} Billion</div><div class="tip-time">${d[0]}</div>`
+});
+svg.call(tip);
 d3.select('.bar-chart').selectAll('g')
   .data(json.data)
   .enter()
   .append('g')
   .attr('transform', el => `translate(${time(new Date(el[0]))}, 0)`)
+  .on('mouseover', tip.show)
   .append('rect')
   .attr('width', (800 / json.data.length) + 1)
   .attr('height', el => 580 - gdp(el[1]))
