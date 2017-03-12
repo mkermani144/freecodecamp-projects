32b5d8d58563e4cd654bb252e5abe77ec72cc5ed
mohammad <mkermani144@gmail.com>
2017-03-12 14:35:38 +0330

    Add axes

--- data-viz/d3/scatterplot-graph/src/index.js
+++ data-viz/d3/scatterplot-graph/src/index.js
@@ -11,18 +11,27 @@ const ranking = d3.scaleLinear()
   .domain([1, 35])
   .range([0, 400])
   .nice();
-const diffTime = d3.scaleLinear()
+const time = d3.scaleLinear()
   .domain([getSeconds(data[0].Time), getSeconds(data[data.length - 1].Time)])
   .range([800, 0])
   .nice();
+const xAxis = d3.axisBottom().scale(time)
+  .tickFormat(timeString =>
+    `${Math.floor(timeString / 60)}:${timeString % 60 < 10 ? '0' : ''}${timeString % 60}`);
+const yAxis = d3.axisLeft().scale(ranking);
 const svg = d3.select('.scatterplot-graph');
-console.log(ranking());
 svg.selectAll('g')
   .data(data)
   .enter()
   .append('circle')
-  .attr('transform', el => `translate(${diffTime(getSeconds(el.Time))}, ${ranking(el.Place)})`)
+  .attr('transform', el => `translate(${time(getSeconds(el.Time))}, ${ranking(el.Place)})`)
   .attr('fill', el => ['green', 'red'][+Boolean(el.Doping)])
   .attr('r', 4)
   .attr('cx', 2)
   .attr('cy', 2);
+svg.append('g')
+  .attr('transform', 'translate(20, 410)')
+  .call(xAxis);
+svg.append('g')
+  .attr('transform', 'translate(20, 10)')
+  .call(yAxis);
