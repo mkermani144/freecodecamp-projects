91f0ef07502fc00c90cfbf463bd5714fb0e09969
mohammad <mkermani144@gmail.com>
2017-04-05 11:30:08 +0430

    Update test to check return value of checkString for unix timestamps

--- backend/timestamp-microservice/src/__tests__/stringChecker.test.js
+++ backend/timestamp-microservice/src/__tests__/stringChecker.test.js
@@ -1,15 +1,15 @@
 const { test } = require('tape');
 const checkString = require('../stringChecker');
-test('stringChecker module functionality on valid dates', (assert) => {
+test('stringChecker module functionality on valid natural dates', (assert) => {
   const actual = checkString('20 March 2017');
   const expected = { unix: 1489955400, natural: 'Mon Mar 20 2017' };
-  assert.deepEqual(actual, expected, 'checkString() should return correct date object for valid dates');
+  assert.deepEqual(actual, expected, 'checkString() should return correct date object for valid natural dates');
   assert.end();
 });
 test('stringChecker module functionality on valid unix timestamps', (assert) => {
-  const actual = checkString('1491289118695');
-  const expected = true;
-  assert.equal(actual, expected, 'checkString() should return true for valid unix timestamps');
+  const actual = checkString('1489955400');
+  const expected = { unix: 1489955400, natural: 'Mon Mar 20 2017' };
+  assert.deepEqual(actual, expected, 'checkString() should return correct date object for valid unix timestamps');
   assert.end();
 });
 test('stringChecker module functionality on empty strings', (assert) => {
