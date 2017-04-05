7691059774b41fd7c911865586a9074ab8a285db
mohammad <mkermani144@gmail.com>
2017-04-05 16:07:19 +0430

    Implement routing for all strings

--- backend/timestamp-microservice/src/index.js
+++ backend/timestamp-microservice/src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const manipulateString = require('./stringManipulator');
 
-app.get('/:unix(\\d+)', (req, res) => {
-  res.send(manipulateString(req.params.unix));
+app.get('/:string', (req, res) => {
+  res.send(manipulateString(req.params.string));
 });
 
-app.listen(8000);
+app.listen(8000, () => console.log('App is running on http://localhost:8000'));
