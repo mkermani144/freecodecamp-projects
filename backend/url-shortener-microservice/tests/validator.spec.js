92542b31015beb02d2bbc3c31bbdd57a6c084e3d
mohammad <mkermani144@gmail.com>
2017-04-06 21:53:44 +0430

    Test validator for valid http urls

--- backend/url-shortener-microservice/tests/validator.spec.js
+++ backend/url-shortener-microservice/tests/validator.spec.js
@@ -3,10 +3,18 @@ const validator = require('../src/validator');
 
 test('validator for no argument', (assert) => {
   const actual = validator();
-  const expected = 'Error: Not a valid URL';
+  const expected = false;
   assert.equal(actual,
     expected,
-    'validator should return error when no argument is given'
+    'validator should return false when no argument is given'
   );
+  assert.end();
+});
+test('validator for valid http url', (assert) => {
+  const actual = validator('http://google.com');
+  const expected = true;
+  assert.equal(actual,
+    expected,
+    'validator should return true when a valid http url is given');
   assert.end();
 });
