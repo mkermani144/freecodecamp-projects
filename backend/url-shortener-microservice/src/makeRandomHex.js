afe519a94bd32b1cd0be4b470b80d37e530530b5
mohammad <mkermani144@gmail.com>
2017-04-07 18:39:00 +0430

    Update makeRandomHex to check for duplicate short urls

--- backend/url-shortener-microservice/src/makeRandomHex.js
+++ backend/url-shortener-microservice/src/makeRandomHex.js
@@ -1,6 +1,11 @@
 const { randomBytes } = require('crypto');
-const makeRandomHex = () => {
-  return randomBytes(3).toString('hex');
+const makeRandomHex = (blacklist) => {
+  while(1) {
+    const randomHex = randomBytes(3).toString('hex');
+    if (blacklist.includes(randomHex) == false) {
+      return randomHex;
+    }
+  }
 }
 
 module.exports = makeRandomHex;
