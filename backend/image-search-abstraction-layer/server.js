29eaa7c04de38ce17570e4412f03e922ff508174
mohammad <mkermani144@gmail.com>
2017-04-08 14:31:44 +0430

    Implement api to send recent queries

--- backend/image-search-abstraction-layer/server.js
+++ backend/image-search-abstraction-layer/server.js
@@ -7,9 +7,20 @@ dotenv.config();
 const app = express();
 const client = new Google(process.env.CSE_ID, process.env.API_KEY);
 
+const recent = Array(5).fill({});
+
 app.get('/api/imagesearch/:query', (req, res) => {
   client.search(req.params.query)
     .then(images => res.send(extract(images)));
+  recent.unshift({
+    query: req.params.query,
+    when: (new Date()).toISOString(),
+  });
+  recent.pop();
+});
+
+app.get('/api/recent/imagesearch', (req, res) => {
+  res.send(recent);
 });
 
 app.listen((process.env.PORT || 8000), () => {
