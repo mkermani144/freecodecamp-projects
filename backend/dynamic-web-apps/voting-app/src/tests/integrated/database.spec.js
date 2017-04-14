e2bd4d1ea50fc79a2f3848c9d85a170d12671250
mohammad <mkermani144@gmail.com>
2017-04-14 18:44:50 +0430

    Update test for adding duplicates to the database

--- backend/dynamic-web-apps/voting-app/src/tests/integrated/database.spec.js
+++ backend/dynamic-web-apps/voting-app/src/tests/integrated/database.spec.js
@@ -1,6 +1,6 @@
 const test = require('tape');
 const User = require('../../../models/User');
-const { connect, add } = require('../../database');
+const { connect, add, remove } = require('../../database');
 
 test('connection to database', async (assert) => {
   const res = await connect();
@@ -10,15 +10,19 @@ test('connection to database', async (assert) => {
 
 test('adding to database', async (assert) => {
   const res = await add(User, 'sample', 'secret');
+  // try {
+    await remove(User, 'sample');
+  // } catch (e) { assert.fail(e); }
   assert.equal(res, 0, 'It should add documents to the database without errors');
-  // await remove(User, 'sample');
   assert.end();
 });
 
 test('adding duplicates to database', async (assert) => {
   await add(User, 'sample', 'secret');
   const res = await add(User, 'sample', 'secret');
-  assert.equal(res, 1, 'It should not add duplicate documents to the database');
-  // await remove(User, 'sample');
+  // try {
+    await remove(User, 'sample');
+  // } catch (e) {}
+  assert.equal(res, 2, 'It should not add duplicate documents to the database');
   assert.end();
 });
