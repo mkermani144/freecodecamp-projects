a882e97f31a23da56792f785f7af068aab8c5147
mohammad <mkermani144@gmail.com>
2017-04-14 16:39:28 +0430

    Test connection to database

---
+++ backend/dynamic-web-apps/voting-app/src/connectToDatabase.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const connectToDatabase = async (mongoose) => {
+  try {
+    await mongoose.connect(process.env.MONGODBURI);
+    console.log('Successfully connected to database');
+    return 0;
+  } catch (e) {
+    console.log('Failed to connect to database: ', e);
+    return 1;
+  }
+}
+
+module.exports = connectToDatabase;
