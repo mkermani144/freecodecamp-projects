55f0f4ab81f18cf33ff62389752ee7571623c27f
mohammad <mkermani144@gmail.com>
2017-04-14 19:07:54 +0430

    Update test file variable names

--- backend/dynamic-web-apps/voting-app/src/tests/integrated/database.spec.js
+++ backend/dynamic-web-apps/voting-app/src/tests/integrated/database.spec.js
@@ -3,26 +3,25 @@ const User = require('../../../models/User');
 const { connect, add, remove } = require('../../database');
 
 test('connection to database', async (assert) => {
-  const res = await connect();
-  assert.equal(res, 0, 'It should connect to the database without errors');
+  const actual = await connect();
+  const expected = 0;
+  assert.equal(actual, expected, 'It should connect to the database without errors');
   assert.end();
 });
 
 test('adding to database', async (assert) => {
-  const res = await add(User, 'sample', 'secret');
-  // try {
-    await remove(User, 'sample');
-  // } catch (e) { assert.fail(e); }
-  assert.equal(res, 0, 'It should add documents to the database without errors');
+  const actual = await add(User, 'sample', 'secret');
+  const expected = 0;
+  await remove(User, 'sample');
+  assert.equal(actual, expected, 'It should add documents to the database without errors');
   assert.end();
 });
 
 test('adding duplicates to database', async (assert) => {
   await add(User, 'sample', 'secret');
-  const res = await add(User, 'sample', 'secret');
-  // try {
-    await remove(User, 'sample');
-  // } catch (e) {}
-  assert.equal(res, 2, 'It should not add duplicate documents to the database');
+  const actual = await add(User, 'sample', 'secret');
+  const expected = 2;
+  await remove(User, 'sample');
+  assert.equal(actual, expected, 'It should not add duplicate documents to the database');
   assert.end();
 });
