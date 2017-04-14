a4f3cff41844002913f95f1fa5cbba33b4797054
mohammad <mkermani144@gmail.com>
2017-04-14 22:40:06 +0430

    Modify test runner

--- backend/dynamic-web-apps/voting-app/src/tests/e2e/database.spec.js
+++ backend/dynamic-web-apps/voting-app/src/tests/e2e/database.spec.js
@@ -1,36 +1,32 @@
-const test = require('tape');
+const test = require('ava');
 const User = require('../../../models/User');
 const { connect, add, remove, findUser } = require('../../database');
 
-test('connection to database', async (assert) => {
+test('connection to database', async (t) => {
   const actual = await connect();
   const expected = 0;
-  assert.equal(actual, expected, 'It should connect to the database without errors');
-  assert.end();
+  t.is(actual, expected, 'It should connect to the database without errors');
 });
 
-test('adding to database', async (assert) => {
+test('adding to database', async (t) => {
   const actual = await add(User, 'sample', 'secret');
   const expected = 0;
   await remove(User, 'sample');
-  assert.equal(actual, expected, 'It should add documents to the database without errors');
-  assert.end();
+  t.is(actual, expected, 'It should add documents to the database without errors');
 });
 
-test('adding duplicates to database', async (assert) => {
+test('adding duplicates to database', async (t) => {
   await add(User, 'sample', 'secret');
   const actual = await add(User, 'sample', 'secret');
   const expected = 2;
   await remove(User, 'sample');
-  assert.equal(actual, expected, 'It should not add duplicate documents to the database');
-  assert.end();
+  t.is(actual, expected, 'It should not add duplicate documents to the database');
 });
 
-test('finding usernames in database', async (assert) => {
+test('finding usernames in database', async (t) => {
   await add(User, 'sample', 'secret');
   const actual = await findUser(User, 'sample');
   const expected = 1;
   await remove(User, 'sample');
-  assert.equal(actual, expected, 'It should tell if the user exists in the database without errors');
-  assert.end();
+  t.is(actual, expected, 'It should tell if the user exists in the database without errors');
 });
--- backend/dynamic-web-apps/voting-app/src/tests/unit/hashPassword.spec.js
+++ backend/dynamic-web-apps/voting-app/src/tests/unit/hashPassword.spec.js
@@ -1,4 +1,4 @@
-const { test } = require('tape');
+const test = require('ava');
 const bcrypt = require('bcrypt');
 const hashPassword = require('../../hashPassword');
 
@@ -7,6 +7,5 @@ test('hasing a password', async (assert) => {
   const hash = await hashPassword(password);
   const actual = await bcrypt.compare(password, hash);
   const expected = true;
-  assert.equal(actual, expected, 'It should hash passwords without error');
-  assert.end();
+  assert.is(actual, expected, 'It should hash passwords without error');
 });
