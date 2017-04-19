aaf67debba094564efe95c89b0fe13f96afb6362
mohammad <mkermani144@gmail.com>
2017-04-19 09:01:24 +0430

    Remove redundant graphql codes

--- backend/dynamic-web-apps/voting-app/server.js
+++ backend/dynamic-web-apps/voting-app/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-// const graphqlHTTP = require('express-graphql');
-// const { buildSchema } = require('graphql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const User = require('./models/User');
 
-// const schema = require('./api/schema');
-// const resolver = require('./api/resolve');
 const db = require('./src/database');
 
 db.connect();
@@ -14,14 +10,6 @@ db.connect();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-// const APISchema = buildSchema(schema);
-
-// app.use('/api', graphqlHTTP({
-//   schema: APISchema,
-//   rootValue: resolver,
-//   graphiql: true,
-// }));
 
 app.post('/api/finduser', async (req, res) => {
   const result = await db.findUser(User, req.body.username);
