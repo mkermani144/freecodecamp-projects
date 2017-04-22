a360aa6f9ed4725860d90385190f57f5210ed2a6
mohammad <mkermani144@gmail.com>
2017-04-22 12:52:05 +0430

    Implement login in the frontend

--- backend/dynamic-web-apps/voting-app/client/src/components/Login.js
+++ backend/dynamic-web-apps/voting-app/client/src/components/Login.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Snackbar from 'material-ui/Snackbar';
 import { blue50, blue500 } from 'material-ui/styles/colors';
 import './Login-Signup.css';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.loginStatus = 0;
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      }),
+    });
+    if (response.ok) {
+      this.setState({
+        loginStatus: 1,
+      });
+    } else if (response.status === 401) {
+      this.setState({
+        loginStatus: 2,
+      });
+    } else {
+      this.setState({
+        loginStatus: 3,
+      });
+    }
+  }
+
+  handleSnackbarClose = () => {
+    this.setState({
+      loginStatus: 0,
+    });
+  }
+
   render() {
     const paperStyle = {
       padding: '5vmin',
@@ -22,20 +78,22 @@ class Login extends Component {
       marginTop: '5vmin',
       alignSelf: 'flex-end'
     };
-    return (
+    return this.state.loginStatus === 1 ? <Redirect to="/" /> : (
       <div className="Login" style={loginStyle}>
         <Paper className="paper" style={paperStyle}>
-          <form action="/login" method="post">
+          <form onSubmit={this.handleSubmit}>
             <TextField
               name="username"
               floatingLabelText="Username"
               floatingLabelFixed={true}
+              onChange={this.handleUsernameChange}
               autoFocus
             />
             <TextField
               name="password"
               type="password"
               floatingLabelText="Password"
+              onChange={this.handlePasswordChange}
               floatingLabelFixed={true}
             />
             <RaisedButton
@@ -46,6 +104,16 @@ class Login extends Component {
             />
           </form>
         </Paper>
+        <Snackbar
+          open={this.state.loginStatus >= 2}
+          message={
+            this.state.loginStatus === 2
+            ? "Wrong username or password. Try again."
+            : "Something bad happended. Try again later."
+          }
+          autoHideDuration={4000}
+          onRequestClose={this.handleSnackbarClose}
+        />
       </div>
     );
   }
