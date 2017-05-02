b297edae31a39f037692d871d8be9cf60c331d0f
mohammad <mkermani144@gmail.com>
2017-05-02 08:59:07 +0430

    Utilize redux

--- backend/dynamic-web-apps/voting-app/client/src/index.js
+++ backend/dynamic-web-apps/voting-app/client/src/index.js
@@ -6,7 +6,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { blue500, amberA700 } from 'material-ui/styles/colors';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './components/App';
+import AppContainer from './components/AppContainer';
 import store from './store';
 import './index.css';
 
@@ -23,7 +23,7 @@ ReactDOM.render((
     <Provider store={store}>
       <MuiThemeProvider muiTheme={customTheme}>
         <Router>
-          <Route path="/" component={App}></Route>
+          <Route path="/" component={AppContainer}></Route>
         </Router>
       </MuiThemeProvider>
     </Provider>
