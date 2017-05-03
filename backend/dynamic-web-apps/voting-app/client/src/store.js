22711792bf0663701d216e31321c6858ef393b54
mohammad <mkermani144@gmail.com>
2017-05-03 08:50:14 +0430

    Add default state to store

--- backend/dynamic-web-apps/voting-app/client/src/store.js
+++ backend/dynamic-web-apps/voting-app/client/src/store.js
@@ -3,13 +3,14 @@ import rootReducer from './reducers/root';
 
 const defaultState = {
   user: {
-    loggedIn: localStorage.get(loggedIn) || false,
+    loggedIn: localStorage.getItem('loggedIn') || false,
   },
   polls: [],
 };
 
 const store = createStore(
   rootReducer,
+  defaultState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
