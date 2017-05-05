470b979a91e507bdfed6dff64cd2db2291cdfbbc
mohammad <mkermani144@gmail.com>
2017-05-05 21:58:56 +0430

    Update AppContainer

--- backend/dynamic-web-apps/voting-app/client/src/components/AppContainer.js
+++ backend/dynamic-web-apps/voting-app/client/src/components/AppContainer.js
@@ -1,15 +1,9 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as actionCreators from '../actions/actionCreators';
 
 import App from './App';
 
-const mapStateToProps = state => state;
+const mapStateToProps = state => ({ loggedIn: state.user.loggedIn });
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(actionCreators, dispatch);
-};
-
-const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
+const AppContainer = connect(mapStateToProps)(App);
 
 export default AppContainer;
