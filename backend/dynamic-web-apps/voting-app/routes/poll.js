cbc5c78a2b2576df41474ae5680e11f2e3c7e907
mohammad <mkermani144@gmail.com>
2017-05-07 12:38:06 +0430

    Fix minor bug in poll router handler

--- backend/dynamic-web-apps/voting-app/routes/poll.js
+++ backend/dynamic-web-apps/voting-app/routes/poll.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/add', async (req, res) => {
   if (req.isAuthenticated()) {
-    const result = await db.addPoll(User, req.user, req.body.poll);
+    const result = await db.addPoll(User, req.user.username, req.body.poll);
     res.json({ successful: result === 0 });
   } else {
     res.json({ successful: false });
