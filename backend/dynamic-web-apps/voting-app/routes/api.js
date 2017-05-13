301c3917b1ca240a1bc938a70aea39e78b1b3ce5
mohammad <mkermani144@gmail.com>
2017-05-13 08:57:31 +0430

    Add sessionIsValid api endpoint

--- backend/dynamic-web-apps/voting-app/routes/api.js
+++ backend/dynamic-web-apps/voting-app/routes/api.js
@@ -13,5 +13,8 @@ router.get('/userpolls/:username', async (req, res) => {
   const result = await db.fetchUserPolls(User, req.params.username);
   res.json({ polls: result });
 });
+router.get('/sessionisvalid', (req, res) => {
+  req.isAuthenticated() ? res.status(200).end() : res.status(401).end();
+});
 
 module.exports = router;
